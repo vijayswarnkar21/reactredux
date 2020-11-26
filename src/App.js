5521f05a74ec1e5411750c1c7a12532e165113b8
vijay kumar swarnkar <vijay.kumarswaranakar@girnarsoft.co.in>
2020-11-26 21:16:27 +0530

    redux with hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Provider store={store}> 
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {buyCake} from '../redux'
+
 const CakeContainer = (props) => {
     return (
         <div>
